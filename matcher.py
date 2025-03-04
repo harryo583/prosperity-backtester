e1e6471a6382397026e0934594a200b0a949f1ff
harryo583 <hongyiwang2027@u.northwestern.edu>
2025-03-04 03:05:00 -0600

    fixed order matching

--- matcher.py
+++ matcher.py
@@ -41,6 +41,8 @@ def match_buy_order(state: TradingState, next_state: TradingState, order: Order)
                 break
             if market_trade.quantity <= 0:
                 continue
+            # if market_trade.timestamp != next_state.timestamp: # make sure market trade is recent
+            #     continue
             if order.price >= market_trade.price: # only match if you're willing to pay higher
                 matched_quantity = min(remaining_quantity, market_trade.quantity)
                 trades.append(
@@ -63,10 +65,11 @@ def match_sell_order(state: TradingState, next_state: TradingState, order: Order
     remaining_quantity = abs(order.quantity) # sell order quantities are negative by convetion
     order_depth = state.order_depths.get(order.symbol)
     
+    # print(order_depth.buy_orders)
+    
     # First fill with order depth
     if order_depth and order_depth.buy_orders:
         eligible_prices = sorted([price for price in order_depth.buy_orders.keys() if price >= order.price], reverse=True)
-        
         for price in eligible_prices:
             available = order_depth.buy_orders[price]
             if available <= 0:
@@ -86,7 +89,8 @@ def match_sell_order(state: TradingState, next_state: TradingState, order: Order
             remaining_quantity -= matched_quantity
             if remaining_quantity == 0:
                 return trades
-
+    # print(remaining_quantity)
+    # print("HEY", market_trades)
     # Fill any remaining quantity with market trades
     if remaining_quantity > 0 and market_trades and order.symbol in market_trades:
         for market_trade in market_trades[order.symbol]:
@@ -94,6 +98,8 @@ def match_sell_order(state: TradingState, next_state: TradingState, order: Order
                 break
             if market_trade.quantity <= 0:
                 continue
+            # if market_trade.timestamp != next_state.timestamp: # make sure market trade is recent
+            #     continue
             if order.price <= market_trade.price: # only match if you're willing to accept lower
                 matched_quantity = min(remaining_quantity, market_trade.quantity)
                 trades.append(
